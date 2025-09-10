6137b196a84d8527fca7bfe97e1f929219f9023e
Jean Carvalho <jeancarvalhodasilva02@gmail.com>
2025-09-09 22:07:39 -0300

    ona

---
+++ src/app/produto/checkout/page.tsx
@@ -0,0 +1,13 @@
+export default function Checkout() {
+
+    return (
+        <section className="checkout" id="checkout">
+            <div>
+                <h2>Infome seu endereço</h2>
+                <div>
+
+                </div>
+            </div>
+        </section>
+    );
+}
